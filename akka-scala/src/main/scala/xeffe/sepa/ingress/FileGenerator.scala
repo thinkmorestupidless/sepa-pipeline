d92a27c04b82bd40c1aeee4f8d519c1a3c476332
Trevor Burton-McCreadie <trevor@lightbend.com>
2020-04-21 10:08:49 +0100

    source transaction generator now sends Monitoring/Control files in different orders

--- akka-scala/src/main/scala/xeffe/sepa/ingress/FileGenerator.scala
+++ akka-scala/src/main/scala/xeffe/sepa/ingress/FileGenerator.scala
@@ -4,7 +4,7 @@ import java.util.UUID
 
 import akka.NotUsed
 import akka.stream.ClosedShape
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Source}
+import akka.stream.scaladsl.{Balance, Broadcast, Flow, GraphDSL, Merge, RunnableGraph, Source}
 import cloudflow.akkastream.scaladsl.RunnableGraphStreamletLogic
 import cloudflow.akkastream.{AkkaStreamlet, AkkaStreamletLogic}
 import cloudflow.streamlets.StreamletShape
@@ -13,6 +13,14 @@ import xeffe.sepa.data.{ControlFile, MonitoringFile, Transaction}
 
 import scala.concurrent.duration._
 
+/**
+ * Simulates the Monitoring files and Control files received by the system.
+ *
+ * Monitoring files represent a single Transaction
+ * Control files contain a list of 1 or more Transactions
+ *
+ * Transactions can be received in any order - Monitoring file first or Control file first.
+ */
 class FileGenerator extends AkkaStreamlet {
 
   val monitoringFileOutlet = AvroOutlet[MonitoringFile]("monitoring-files")
@@ -26,26 +34,38 @@ class FileGenerator extends AkkaStreamlet {
       RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
         import GraphDSL.Implicits._
 
-        val transactions = Source.tick(10 seconds, 1 second, NotUsed)
+        // Source
+        val transactions = Source.tick(10 seconds, 100 milliseconds, NotUsed)
           .map { _ =>
             val txId = UUID.randomUUID().toString
             Transaction(txId, "", "", 0)
           }
 
-        val broadcast = builder.add(Broadcast[Transaction](2))
-
-        val txToMonitoringFile = Flow[Transaction].map { tx => MonitoringFile(tx, s"$tx.id") }
-        val groupTx = Flow[Transaction].grouped(5)
-        val txToControlFile = Flow[Seq[Transaction]].map {
-          val ctrlId = UUID.randomUUID().toString
-          txSeq => ControlFile(ctrlId, s"$ctrlId.json", txSeq)
-        }
-
+        // Sinks
         val monitoringFilesOut = plainSink(monitoringFileOutlet)
         val controlFilesOut = plainSink(controlFileOutlet)
 
-        transactions ~> broadcast ~> txToMonitoringFile ~> monitoringFilesOut
-                        broadcast ~> groupTx            ~> txToControlFile       ~> controlFilesOut
+        // Structure
+        val balance = builder.add(Balance[MonitoringFile](2))
+        val merge = builder.add(Merge[MonitoringFile](2))
+        val broadcast = builder.add(Broadcast[Transaction](2))
+
+        // Flows
+        val txToMonitoringFile = Flow[Transaction].map { tx =>
+          MonitoringFile(tx, s"$tx.id")
+        }
+        val delayedMonitoringFile = Flow[MonitoringFile].delay(5 seconds)
+        val groupTx = Flow[Transaction].grouped(5)
+        val txSeqToControlFile = Flow[Seq[Transaction]].map { txSeq =>
+          val ctrlId = UUID.randomUUID().toString
+          ControlFile(ctrlId, s"$ctrlId.json", txSeq)
+        }
+
+        // The Graph
+
+        transactions ~> broadcast ~> txToMonitoringFile ~> balance ~> delayedMonitoringFile ~> merge ~> monitoringFilesOut
+                                                           balance                          ~> merge
+                        broadcast ~> groupTx ~> txSeqToControlFile ~> controlFilesOut
 
         ClosedShape
       })
